c89fdf2122e0c6bca580f5810593d9446199de7e
lanzy-lanzy <79232798+lanzy-lanzy@users.noreply.github.com>
2023-06-28 22:17:06 +0800

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-from django.contrib.sessions.models import Session
+
 
 # Create your views here.
 
 def landing_page(request):
     return render(request, 'landing_page.html')
-    
-def visitor_count(request):
-    visitor_count = Session.objects.count()
-    return {
-        'visitor_count': visitor_count,
-    }
-
